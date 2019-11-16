254212c2e8ea817e2adc0a6cb4c7665019c78e2c
Khalifa Ben Lazrek <khalifa@Khalifas-MacBook-Pro.local>
2019-11-16 12:47:32 +0100

    Updates

---
+++ precache-manifest.5e9e2897a7a3c22049eed1ad0971399e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16939683e66783fb54cd3dff0a7ef513",
+    "url": "/react-season-weather/index.html"
+  },
+  {
+    "revision": "98d2b78aed3649a24b04",
+    "url": "/react-season-weather/static/css/main.bf5bb20e.chunk.css"
+  },
+  {
+    "revision": "8d05c72e573b7bebae5e",
+    "url": "/react-season-weather/static/js/2.73871404.chunk.js"
+  },
+  {
+    "revision": "98d2b78aed3649a24b04",
+    "url": "/react-season-weather/static/js/main.4d6a1fa7.chunk.js"
+  },
+  {
+    "revision": "b72dd34efe7568cb0634",
+    "url": "/react-season-weather/static/js/runtime-main.be26b285.js"
+  }
+]);
